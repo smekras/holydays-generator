587db5cedfc71cd5d036608121e1bd61119f8a0f
Stergios Mekras <stergios.mekras@gmail.com>
2020-02-24 23:58:55 +0100

    Add up to 12/03

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main():
 
     for i in days:
         # temp check until all the generators are done
-        if i.month > 3 or (i.month == 3 and i.day > 10):
+        if i.month > 3 or (i.month == 3 and i.day > 12):
             religious = [0]
             names = [0]
             secular = [0]
