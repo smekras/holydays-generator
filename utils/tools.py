ea54af9274020a47381c1e49b751f5acac5d919a
Stergios Mekras <stergios.mekras@gmail.com>
2020-07-27 15:30:50 +0200

    Add json writer

--- utils/tools.py
+++ utils/tools.py
@@ -1,3 +1,5 @@
+import csv
+import json
 from dicts import holidays as h
 
 temp = {}
@@ -8,3 +10,20 @@ def check_doubles():
         temp.setdefault(value, set()).add(key)
 
     [print(key) for key, values in temp.items() if len(values) > 1]
+
+
+def csv_to_json(csv_file, json_file):
+    data = []
+
+    with open(csv_file) as f:
+        for row in csv.DictReader(f):
+            data.append(row)
+
+    json_data = json.dumps(data)
+
+    with open(json_file, "w+") as f:
+        f.write(json_data)
+    # json.dump(json_data, json_file)
+
+
+csv_to_json('../files/2020.csv', '../files/2020.json')
